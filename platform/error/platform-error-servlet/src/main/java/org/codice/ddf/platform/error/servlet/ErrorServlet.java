d7b69f262de9115187d63765b47af9908065553a
brjeter <brjeter@users.noreply.github.com>
2018-11-06 16:59:51 -0800

    Revert "DDF-4123/4279/4284 Minor forward ports (#3997)"
    This reverts commit 00c7820faf6ee267cc84291f8f65b9dc377362d7.

--- platform/error/platform-error-servlet/src/main/java/org/codice/ddf/platform/error/servlet/ErrorServlet.java
+++ platform/error/platform-error-servlet/src/main/java/org/codice/ddf/platform/error/servlet/ErrorServlet.java
@@ -14,6 +14,7 @@
 package org.codice.ddf.platform.error.servlet;
 
 import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -66,16 +67,8 @@ public class ErrorServlet extends HttpServlet {
   }
 
   @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-    handleError(request, response);
-  }
-
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-    handleError(request, response);
-  }
-
-  private void handleError(HttpServletRequest request, HttpServletResponse response) {
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
     Object codeObj = request.getAttribute(ERROR_STATUS_CODE);
     Object messageObj = request.getAttribute(ERROR_MESSAGE);
     Object typeObj = request.getAttribute(ERROR_EXCEPTION_TYPE);
